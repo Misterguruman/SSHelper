3b2027f4464e4f3a3b188ccd2b296ee0e6ecbdf6
Misterguruman <joseph.p.langford@gmail.com>
2016-02-08 15:42:16 -0500

    Added basic SSH shell for testing

--- SSHelper.py
+++ SSHelper.py
@@ -28,3 +28,13 @@ ssh.set_missing_host_key_policy(
 			paramiko.AutoAddPolicy())
 ssh.connect('127.0.0.1', username= "joseph", password= "")
 
+data=""
+while data is not "8":
+
+	data = raw_input("SSH> ")
+	stdin, stdout, stderr = ssh.exec_command(data)
+	type(stdin)
+
+	for lines in stdout.readlines():
+		print lines
+
