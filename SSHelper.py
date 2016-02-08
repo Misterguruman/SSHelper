9ad9c8cbad11ef57cad3c1e0dd84ae5e7b8bd945
Misterguruman <joseph.p.langford@gmail.com>
2016-02-08 13:13:48 -0500

    Added basic connection to SSHelper to test paramiko on localhost

--- SSHelper.py
+++ SSHelper.py
@@ -5,28 +5,26 @@ import sys
 def print_usage():
 	#Prints usage of SSHelper
 	print("""
-SSHelper Utility
---------------------------------------------
-Author : Joseph Paul Langford
-Version 0.1
+	SSHelper Utility
+	--------------------------------------------
+	Author : Joseph Paul Langford
+	Version 0.1
 
-Usage:
+	Usage:
 
-SSHelper required 3 arguments:
+	SSHelper required 3 arguments:
 	The IP address/dns name of the remote host
 	The Username of the remote host
 	The password of the remote host
 
-Examples:
+	Examples:
 	./SSHelper 127.0.0.1 admin supersecretpassword
 	python SSHelper.py my.dns.com p@ssW0rd	
 		""")
 
-print_usage()
-#Creating Client Instance
-#ssh = paramiko.SSHClient()
+ssh = paramiko.SSHClient()
 
-#ssh.set_missing_host_key_policy(
-#	paramiko.AutoAddPolicy())
+ssh.set_missing_host_key_policy(
+			paramiko.AutoAddPolicy())
+ssh.connect('127.0.0.1', username= "joseph", password= "")
 
-#ssh.connect(ip, username=usr, password=pwd)
