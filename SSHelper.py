95cf48538bbef8ee7572443f0a57b9ffdbdf2709
Joseph Langford <joseph.p.langford@gmail.com>
2016-02-09 18:34:57 -0500

    Added previous main to 'basic_shell' function so that can be called later after argv parsing

--- SSHelper.py
+++ SSHelper.py
@@ -5,46 +5,45 @@ import sys
 def print_usage():
 	#Prints usage of SSHelper
 	print("""
-SSHelper Utility
---------------------------------------------
-Author : Joseph Paul Langford
-Version 0.1
+ SSHelper Utility
+ --------------------------------------------
+ Author : Joseph Paul Langford
+ Version 0.1
 
-Usage:
+ Usage:
 
-Default:
+ Default:
 	Without additional arguments SSHelper acts as a basic shell,
 	best for basic system commands that don't require changing directories.
 
-Default SSHelper required 3 arguments:
+ Default SSHelper required 3 arguments:
 
-The IP address/dns name of the remote host
-The Username of the remote host
-The password of the remote host
+ The IP address/dns name of the remote host
+ The Username of the remote host
+ The password of the remote host
 
-Examples:
-./SSHelper 127.0.0.1 admin supersecretpassword
-python SSHelper.py my.dns.com p@ssW0rd	
+ Examples:
+ ./SSHelper 127.0.0.1 admin supersecretpassword
+ python SSHelper.py my.dns.com p@ssW0rd	
 		""")
-ip, usr, pwd = sys.argv[1:4]
-print ip
-print usr
-print pwd
+	
+def basic_shell():
+	ip, usr, pwd = sys.argv[1:4]
 
-ssh = paramiko.SSHClient()
+	ssh = paramiko.SSHClient()
 
-ssh.set_missing_host_key_policy(
+	ssh.set_missing_host_key_policy(
 			paramiko.AutoAddPolicy())
-ssh.connect(ip, username= usr, password= pwd)
+	ssh.connect(ip, username= usr, password= pwd)
 
-data=""
+	data=""
 
-while data is not "8":
+	while data is not "8":
 
-	data = raw_input("SSH> ")
-	stdin, stdout, stderr = ssh.exec_command(data)
-	type(stdin)
+		data = raw_input("SSH> ")
+		stdin, stdout, stderr = ssh.exec_command(data)
+		type(stdin)
 
-	for lines in stdout.readlines():
-		print lines
+		for lines in stdout.readlines():
+			print lines
 
