c52f98f842cea08e00703740f9c3c10b810ee401
Joseph Langford <joseph.p.langford@gmail.com>
2016-02-09 18:15:01 -0500

    Added Basic functionality to usage, added argv parsing for basic shell

--- SSHelper.py
+++ SSHelper.py
@@ -5,30 +5,40 @@ import sys
 def print_usage():
 	#Prints usage of SSHelper
 	print("""
-	SSHelper Utility
-	--------------------------------------------
-	Author : Joseph Paul Langford
-	Version 0.1
+SSHelper Utility
+--------------------------------------------
+Author : Joseph Paul Langford
+Version 0.1
 
-	Usage:
+Usage:
 
-	SSHelper required 3 arguments:
-	The IP address/dns name of the remote host
-	The Username of the remote host
-	The password of the remote host
+Default:
+	Without additional arguments SSHelper acts as a basic shell,
+	best for basic system commands that don't require changing directories.
 
-	Examples:
-	./SSHelper 127.0.0.1 admin supersecretpassword
-	python SSHelper.py my.dns.com p@ssW0rd	
+Default SSHelper required 3 arguments:
+
+The IP address/dns name of the remote host
+The Username of the remote host
+The password of the remote host
+
+Examples:
+./SSHelper 127.0.0.1 admin supersecretpassword
+python SSHelper.py my.dns.com p@ssW0rd	
 		""")
+ip, usr, pwd = sys.argv[1:4]
+print ip
+print usr
+print pwd
 
 ssh = paramiko.SSHClient()
 
 ssh.set_missing_host_key_policy(
 			paramiko.AutoAddPolicy())
-ssh.connect('127.0.0.1', username= "joseph", password= "")
+ssh.connect(ip, username= usr, password= pwd)
 
 data=""
+
 while data is not "8":
 
 	data = raw_input("SSH> ")
